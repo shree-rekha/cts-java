9ada5531f8d13261d6b72bc9d309dd184135f5fa
shree-rekha <shreerekha.msec@gmail.com>
2025-05-22 16:06:29 +0530

    Add files via upload

---
+++ ArraySumAndAverage.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArraySumAndAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user to enter the number of elements
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        // Create an array of size n
+        int[] numbers = new int[n];
+        int sum = 0;
+
+        // Read elements into the array
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+        }
+
+        // Calculate average
+        double average = (double) sum / n;
+
+        // Display sum and average
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+        scanner.close();
+    }
+}
---
+++ InheritanceExample.java
@@ -0,0 +1,28 @@
+// Base class
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// Subclass
+class Dog extends Animal {
+    // Overriding the makeSound() method
+    @Override
+    void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+// Main class
+public class InheritanceExample {
+    public static void main(String[] args) {
+        // Creating an object of the base class
+        Animal myAnimal = new Animal();
+        myAnimal.makeSound();  // Output: Animal makes a sound
+
+        // Creating an object of the subclass
+        Dog myDog = new Dog();
+        myDog.makeSound();     // Output: Bark
+    }
+}
